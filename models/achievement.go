4c4d26328b0ec8b75dcf1564a5b82926d2440b8b
kash / Hytus <kash@hytus.net>
2021-04-06 02:49:41 +0900

    :bug: fix type mismatch (`uint8` to `uint32`)
    uint8 と uint32 を間違えました
    
    Signed-off-by: kash / Hytus <kash@hytus.net>

--- models/achievement.go
+++ models/achievement.go
@@ -1,7 +1,7 @@
 package models
 
 type ClubAchievement struct {
-	AchievementID uint8  `gorm:"type:int unsigned;not null;primaryKey;autoIncrement"`
+	AchievementID uint32 `gorm:"type:int unsigned;not null;primaryKey;autoIncrement"`
 	UUID          string `gorm:"type:char(36);not null"`
 	Achievement   string `gorm:"type:text;not null"`
 }
