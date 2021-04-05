112fc8f2021d065579ae73dc452a9c728831a6f0
kash / Hytus <kash@hytus.net>
2021-04-06 01:55:25 +0900

    :card_file_box: add not null and unique tags to some models not to cause bugs
    バグを引き起こさないようにいくつかのモデルのフィールドに not null または unique 属性をつけた
    
    Signed-off-by: kash / Hytus <kash@hytus.net>

--- models/activity.go
+++ models/activity.go
@@ -3,5 +3,5 @@ package models
 type ClubActivity struct {
 	ActivityID uint8  `gorm:"type:int unsigned;not null;primaryKey;autoIncrement"`
 	UUID       string `gorm:"type:char(36);not null"`
-	Activity   string `gorm:"type:text"`
+	Activity   string `gorm:"type:text;not null;unique"`
 }
--- models/image.go
+++ models/image.go
@@ -3,5 +3,5 @@ package models
 type ClubImage struct {
 	ImageID uint8  `gorm:"type:int unsigned;not null;primaryKey;autoIncrement"`
 	UUID    string `gorm:"type:char(36);not null"`
-	Path    string `gorm:"type:text;unique"`
+	Path    string `gorm:"type:text;not null;unique"`
 }
