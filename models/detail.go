281a12f0cd322309369c16ac826cf5aebb549ab8
kash / Hytus <kash@hytus.net>
2021-04-09 14:10:19 +0900

    :label: change remarks field type from `string` to `sql.NullString`
    Signed-off-by: kash / Hytus <kash@hytus.net>

--- models/detail.go
+++ models/detail.go
@@ -1,18 +1,20 @@
 package models
 
+import "database/sql"
+
 type ClubTime struct {
-	TimeID    uint32      `gorm:"type:int unsigned not null auto_increment;primaryKey"`
-	Date      string      `gorm:"type:varchar(3);not null;unique"`
-	Time      string      `gorm:"type:varchar(255);not null;unique"`
-	Remarks   string      `gorm:"type:text"`
-	ClubPlace []ClubPlace `gorm:"many2many:activity_details;foreignKey:TimeID;joinForeignKey:TimeID;references:PlaceID;joinReferences:PlaceID"`
+	TimeID    uint32         `gorm:"type:int unsigned not null auto_increment;primaryKey"`
+	Date      string         `gorm:"type:varchar(3);not null;unique"`
+	Time      string         `gorm:"type:varchar(255);not null;unique"`
+	Remarks   sql.NullString `gorm:"type:text"`
+	ClubPlace []ClubPlace    `gorm:"many2many:activity_details;foreignKey:TimeID;joinForeignKey:TimeID;references:PlaceID;joinReferences:PlaceID"`
 }
 
 type ClubPlace struct {
-	PlaceID  uint32     `gorm:"type:int unsigned not null auto_increment;primaryKey"`
-	Place    string     `gorm:"type:text;not null;unique"`
-	Remarks  string     `gorm:"type:text"`
-	ClubTime []ClubTime `gorm:"many2many:activity_details;foreignKey:PlaceID;joinForeignKey:PlaceID;references:TimeID;joinReferences:TimeID"`
+	PlaceID  uint32         `gorm:"type:int unsigned not null auto_increment;primaryKey"`
+	Place    string         `gorm:"type:text;not null;unique"`
+	Remarks  sql.NullString `gorm:"type:text"`
+	ClubTime []ClubTime     `gorm:"many2many:activity_details;foreignKey:PlaceID;joinForeignKey:PlaceID;references:TimeID;joinReferences:TimeID"`
 }
 
 type ActivityDetail struct {
