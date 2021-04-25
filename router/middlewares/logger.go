44f5a538b249ae84de10d6f7a6942c8de9957afa
kash / Hytus <kash@hytus.net>
2021-04-25 19:44:44 +0900

    :sparkles: add customized logging configs, yay :truck: move JST var to consts package
    Signed-off-by: kash / Hytus <kash@hytus.net>

--- router/middlewares/logger.go
+++ router/middlewares/logger.go
@@ -2,11 +2,10 @@ package middlewares
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/lc-tut/club-portal/consts"
 	"go.uber.org/zap"
 	"time"
 )
-
-var JST = time.FixedZone("Asia/Tokyo", 9*60*60)
 
 // LoggerMiddleware アクセスごとにログを記録する.
 //
@@ -14,13 +13,13 @@ var JST = time.FixedZone("Asia/Tokyo", 9*60*60)
 // 標準時を JST に強制変換させている.
 func LoggerMiddleware(logger *zap.Logger) gin.HandlerFunc {
 	return func(ctx *gin.Context) {
-		start := time.Now().In(JST)
+		start := time.Now().In(consts.JST)
 
 		path := ctx.Request.URL.Path
 		query := ctx.Request.URL.RawQuery
 		ctx.Next()
 
-		end := time.Now().In(JST)
+		end := time.Now().In(consts.JST)
 		latency := end.Sub(start)
 
 		if len(ctx.Errors) > 0 {
