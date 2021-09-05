73efab1477f1151d847d3f8257cdc2d0c5684138
kash / Hytus <kash@hytus.net>
2021-09-05 17:01:08 +0900

    :zap: implement get router
    Signed-off-by: kash / Hytus <kash@hytus.net>

--- repos/admins/repo.go
+++ repos/admins/repo.go
@@ -6,6 +6,7 @@ import (
 )
 
 type IAdminRepository interface {
+	AdminUserRepo
 }
 
 type AdminRepository struct {
--- router/v1/admins/user.go
+++ router/v1/admins/user.go
@@ -1,9 +1,20 @@
 package admins
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/lc-tut/club-portal/consts"
+	"net/http"
+)
 
 func (h *handler) GetUserFromAdmin() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
+		userUUID := ctx.GetString(consts.UserUUIDKeyName)
+		user, err := h.repo.GetSpecifiedUser(userUUID)
 
+		if err != nil {
+			ctx.Status(http.StatusInternalServerError)
+		} else {
+			ctx.JSON(http.StatusOK, user.ToUserResponse())
+		}
 	}
 }
