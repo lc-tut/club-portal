371c584bc3bdf7f8d45680090bc8f68fc91ef91d
Mikuto <mikuto1227k@gmail.com>
2025-03-16 23:42:28 +0900

    change:SavePointの削除

--- repos/clubs/page.go
+++ repos/clubs/page.go
@@ -251,8 +251,6 @@ func (r *ClubRepository) CreatePage(uuid string, args ClubPageCreateArgs) (*club
 
 	err := r.db.Transaction(func(tx *gorm.DB) error {
 
-		tx.SavePoint("CreatePage_init_point")
-
 		if err := tx.Create(page).Error; err != nil {
 			return err
 		}
@@ -287,19 +285,13 @@ func (r *ClubRepository) CreatePage(uuid string, args ClubPageCreateArgs) (*club
 
 		if err := r.CreatePlaceWithTx(tx, args.Places); err != nil {
 			return err
-		}
-		
-		// 一度トランザクションをsave
-		tx.SavePoint("CreatePage_save")
-		
+		}		
 
 		if err := r.CreateActivityDetailWithTx(tx, uuid, args.ActivityDetails); err != nil {
-			tx.RollbackTo("CreatePage_init_point")
 			return err
 		}
 
 		if err := r.CreateTPRemarkWithTx(tx, uuid, args.TPRemark); err != nil {
-			tx.RollbackTo("CreatePage_init_point")
 			return err
 		}
 
